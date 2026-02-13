c7ea7449a15807c1a34dc8c45cb47340f80191c6
Allan Ratemo <allanisratemo@gmail.com>
2026-02-13 09:28:50 +0300

    Clear migration history before upgrade

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Stamp database to current state and run migrations
-flask db stamp head || true
-flask db upgrade || flask db stamp head
+# Clear migration history and apply fresh migrations
+flask db stamp --purge 2>/dev/null || true
+flask db upgrade
